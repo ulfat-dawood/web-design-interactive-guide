3e89b09bd12f1c620ad5165472a1f9f359ce9b64
ulfat-dawood <ulfat_dawood@outlook.com>
2021-01-10 01:25:11 +0300

    final

--- animation/sandbox/app.js
+++ animation/sandbox/app.js
@@ -1,70 +1,73 @@
-const box= document.querySelector('#box');
-const play= document.querySelector("#play");
-const remove= document.querySelector("#remove");
-const allRadio= document.querySelectorAll('input');
+const box = document.querySelector('#box');
+const play = document.querySelector("#play");
+const remove = document.querySelector("#remove");
+const reset = document.querySelector("#reset");
+const allRadio = document.querySelectorAll('input');
 
 
 //All Radio:
-allRadio.forEach((button)=>{
+allRadio.forEach((button) => {
     button.addEventListener('click', () => {
 
         //set the property
-        const property= button.name;
-        const value= button.value;
+        const property = button.name;
+        const value = button.value;
         //remove the animation
-        if(box.classList.contains('anim') && property=='iteration'){
+        if (property != 'play-state')  {
             box.classList.remove('anim');
+            setTimeout(function () {
+                box.classList.add('anim');
+            }, 10);
         }
-        
-        switch(property){
+
+        switch (property) {
             case 'duration':
-                box.style.animationDuration=value;
+                box.style.animationDuration = value;
                 break;
             case 'func':
-                box.style.animationTimingFunction=value;
+                box.style.animationTimingFunction = value;
                 break;
             case 'delay':
-                box.style.animationDelay=value;
+                box.style.animationDelay = value;
                 break;
             case 'iteration':
-                box.style.animationIterationCount=value;
+                box.style.animationIterationCount = value;
                 break;
             case 'direction':
-                box.style.animationDirection=value;
+                box.style.animationDirection = value;
                 break;
             case 'fill-mode':
-                box.style.animationFillMode=value;
+                box.style.animationFillMode = value;
                 break;
             case 'play-state':
-                box.style.animationPlayState=value;
+                box.style.animationPlayState = value;
                 break;
         }
 
-      });
-}) 
+    });
+})
 
 
-// remove.addEventListener('click',remove(()=>{
-//     box.classList.add('anim');
-// }))
 
-// function remove( callback) { 
-//     if(box.classList.contains('anim')){
-//         box.classList.remove('anim');
-//     } 
-//     if (typeof callback == "function") 
-//         callback(); 
-// } 
-
-
-//Remove button
-remove.addEventListener('click',()=>{
-    if(box.classList.contains('anim')){
+remove.addEventListener('click', () => {
+    if (box.classList.contains('anim')) {
         box.classList.remove('anim');
     }
 })
 
-play.addEventListener('click',()=>{
-    box.classList.add('anim');
+play.addEventListener('click', () => {
+    box.classList.remove('anim');
+    setTimeout(function () {
+        box.classList.add('anim');
+    }, 10);
+     
+})
+
+
+reset.addEventListener('click', () => {
+    box.removeAttribute('style');
+    allRadio.forEach((button)=>{
+        button.checked= false; 
+    })
 })
 
