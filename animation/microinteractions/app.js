517df1fc480df460654ec0a8bff754b44c316e61
ulfat-dawood <ulfat_dawood@outlook.com>
2021-01-12 04:29:13 +0300

    final

--- animation/microinteractions/app.js
+++ animation/microinteractions/app.js
@@ -9,3 +9,20 @@ send.addEventListener('click', () => {
     }, 10);
      
 })
+
+
+
+//PLAY BOTTON:
+const btn= document.querySelector('#play .btn');
+const topp= document.querySelector(".s-line.top");
+const bottom= document.querySelector(".s-line.bottom");
+const text= document.querySelector("#play .text");
+
+btn.addEventListener('click', () => {
+    console.log("HI");
+    topp.classList.toggle('rotate-top');
+    bottom.classList.toggle('rotate-bottom');
+    text.innerHTML= text.innerHTML== 'PLAY'? "PAUSE": 'PLAY'; 
+
+     
+})
