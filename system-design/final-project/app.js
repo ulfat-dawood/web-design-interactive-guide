52e979141b2bd9dbc1584e55ef251116fdff5496
ulfat-dawood <ulfat_dawood@outlook.com>
2021-03-01 03:37:14 +0300

    image changes

--- system-design/final-project/app.js
+++ system-design/final-project/app.js
@@ -2,6 +2,7 @@ const checkbox= document.querySelector('#menu-checkbox');
 const sidebar= document.querySelector('aside')
 
 checkbox.addEventListener('click', ()=>{
+    //Store the state in the application memory: 
     checkbox.checked ? 
     sidebar.classList.add('collapse')
     :
