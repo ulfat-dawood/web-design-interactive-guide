e7615a2180fa35f731c31f73bb724401dab6fcff
ulfat-dawood <ulfat_dawood@outlook.com>
2021-03-03 16:28:29 +0300

    final

--- system-design/final-project/dashboard/app.js
+++ system-design/final-project/dashboard/app.js
@@ -1,15 +1,28 @@
 const checkbox= document.querySelector('#menu-checkbox');
-const sidebar= document.querySelector('aside')
+const sidebar= document.querySelector('aside');
+const overlay= document.querySelector('.overlay');
 
  
 const toggleSideBar= ()=>{
-    //Store the state in the application memory: 
-    console.log(checkbox.checked)
-    checkbox.checked ? 
-    sidebar.classList.add('collapse')
-    :
+    if(checkbox.checked){
+    sidebar.classList.add('collapse');
+    overlay.classList.remove('active');
+    }else{
     sidebar.classList.remove('collapse');
+    overlay.classList.add('active');
+    }
 }
+
+//Event listeners
 window.addEventListener('load', toggleSideBar)
 checkbox.addEventListener('click', toggleSideBar)   
+overlay.addEventListener('click',()=>{
+    checkbox.checked= !checkbox.checked; 
+    toggleSideBar(); 
+})
+
+
+
+
+
 
