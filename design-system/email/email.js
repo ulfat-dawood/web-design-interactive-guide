e15d9dbed8f83adcf063947180d0021441ed5c59
ulfat-dawood <ulfat_dawood@outlook.com>
2021-03-05 05:15:23 +0300

    email final

--- design-system/email/email.js
+++ design-system/email/email.js
@@ -1,22 +1,22 @@
 const inbox= document.querySelector('#inbox')
 
 const emails= [
-    {sender:"Sara" , heading:"fermentum dui faucibus. Mauris ultrices eros", date:'Jan 10'},
-    {sender:"Microsoft Team" , heading:"condimentum vitae sapien. At lectus urna duis convallis", date:'Jan 19'},
-    {sender:"Dale A." , heading:"At lectus urna duis convallis convallis.", date:'Feb 29'},
-    {sender:"Nick Wells" , heading:"Augue eget arcu dictum varius duis at consectetur lorem donec.", date:'Feb 22'},
-    {sender:"Jim Berny" , heading:"Pulvinar elementum integer enim neque.", date:'Apr 3'},
-    {sender:"Facebook" , heading:"Vitae proin sagittis nisl rhoncus", date:'Apr 11'},
-    {sender:"Dave" , heading:"Sem et tortor consequat id porta nibh ", date:'Apr 26'},
-    {sender:"M. T" , heading:"Vitae purus faucibus ornare suspendisse", date:'May 5'},
-    {sender:"Morty" , heading:"Morbi blandit cursus risus at ultrices.", date:'May 10'},
-    {sender:"Jawad " , heading:"Velit euismod in pellentesque ", date:'May 14'},
-    {sender:"Keith from Plex" , heading:"pellentesque elit ullamcorper dignissim cras", date:'May 20'},
-    {sender:"Mike Andrew" , heading:"Iaculis at erat pellentesque adipiscing commodo ", date:'Jun 26'},
-    {sender:"Audible.com" , heading:"Eu facilisis sed odio morbi quis", date:'Jun 25'},
-    {sender:"Omar Shaik" , heading:" Viverra maecenas accumsan lacus vel.", date:'Jun 28'},
-    {sender:"Apple" , heading:"Viverra ipsum nunc aliquet ", date:'Aug 28'},
-    {sender:"Slack" , heading:"Ultrices dui sapien eget mi", date:'Aug 27'}
+    {sender:"Sara" , heading:"fermentum dui faucibus.", date:'Jan 10'},
+    {sender:"Microsoft Team" , heading:"condimentum vitae sapien. ", date:'Jan 19'},
+    {sender:"Dale A." , heading:"At lectus urna duis", date:'Feb 29'},
+    {sender:"Nick Wells" , heading:"Augue eget arcu dictum ", date:'Feb 22'},
+    {sender:"Jim Berny" , heading:"Pulvinar elementum.", date:'Apr 3'},
+    {sender:"Facebook" , heading:"Vitae proin ", date:'Apr 11'},
+    {sender:"Dave" , heading:"Sem et tortor ", date:'Apr 26'},
+    {sender:"M. T" , heading:"Vitae purus faucibus ", date:'May 5'},
+    {sender:"Morty" , heading:"Morbi blandit.", date:'May 10'},
+    {sender:"Jawad " , heading:"Velit euismod ", date:'May 14'},
+    {sender:"Keith from Plex" , heading:"pellentesque ", date:'May 20'},
+    {sender:"Mike Andrew" , heading:"Iaculis at erat ", date:'Jun 26'},
+    {sender:"Audible.com" , heading:"Eu facilisis ", date:'Jun 25'},
+    {sender:"Omar Shaik" , heading:" Viverra maecenas.", date:'Jun 28'},
+    {sender:"Apple" , heading:"Viverra ipsum ", date:'Aug 28'},
+    {sender:"Slack" , heading:"Ultrices dui ", date:'Aug 27'}
 ]
 var msg= '';
 emails.forEach((email,index)=>{
