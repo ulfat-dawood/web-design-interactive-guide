e887b98adeb1ded29bbfbef1f148a33f96f78e6d
ulfat-dawood <ulfat_dawood@outlook.com>
2021-03-11 03:13:13 +0300

    pause dashboard

--- design-system/dashboard/dashboard.js
+++ design-system/dashboard/dashboard.js
@@ -1,10 +1,20 @@
 const annualProgress= document.querySelector('#annual-progress').getContext('2d');
 
+//Global Options:
+Chart.defaults.global.defaultFontFamily='Roboto';  
+
 let chart= new Chart(annualProgress , {
-    type: 'bar', //horizontalBar , pie , line , doughnut , radar , polarArea
+    type: 'line', //horizontalBar , pie , line , doughnut , radar , polarArea
     data: {
-        labels:[], 
-        datasets:[]
+        labels:['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'], 
+        //we can have multiple data sets
+        datasets:[{
+            label:'population',
+            data:[2,5,6,9,11,15,20,24,27,29,34,37]
+        }]
     }, 
-    options: {}
+    options: {
+        title:{display:false},
+        legend:{display:false}
+    }
 })
