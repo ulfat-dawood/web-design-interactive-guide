88fee115c252468dc96ee3bb5f726d79e77739f4
ulfat-dawood <ulfat_dawood@outlook.com>
2021-03-19 02:01:13 +0300

    modal done

---
+++ design-system/components/components.js
@@ -0,0 +1,17 @@
+const openModal= document.querySelector('[data-open-modal]');
+const closeModal= document.querySelectorAll('[data-close-modal]');
+const modalOverlay= document.querySelector('.modal-overlay');
+
+openModal.addEventListener('click', ()=>{
+    const modal= document.querySelector(openModal.dataset.openModal);
+    modal.classList.add('active')
+    modalOverlay.classList.add('active')
+})
+
+closeModal.forEach((btn)=>{
+    btn.addEventListener('click',()=>{
+        const modal= document.querySelector(btn.dataset.closeModal);
+        modal.classList.remove('active')
+        modalOverlay.classList.remove('active')
+    })
+})
